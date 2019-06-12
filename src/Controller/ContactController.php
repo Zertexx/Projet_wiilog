7c721236021d8f7134f275b0d7617658d5bf78c6
Maxime Vasseur <zertouflex@gmail.com>
2019-06-12 23:39:38 +0200

    contacts ok

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -28,7 +28,7 @@ class ContactController extends AbstractController
     }
 
     /**
-     * @Route("/new", name="contact_new", methods={"GET","POST"})
+     * @Route("/ajouter", name="contact_new", methods={"GET","POST"})
      */
     public function new(Request $request): Response
     {
@@ -61,7 +61,7 @@ class ContactController extends AbstractController
     }
 
     /**
-     * @Route("/{id}/edit", name="contact_edit", methods={"GET","POST"})
+     * @Route("/{id}/editer", name="contact_edit", methods={"GET","POST"})
      */
     public function edit(Request $request, Contact $contact): Response
     {
