d958ef8a3f68c5e620c14353d32938c78c4e9eb5
Maxime Vasseur <zertouflex@gmail.com>
2019-06-13 00:09:07 +0200

    ajout type

--- src/Form/EnterpriseType.php
+++ src/Form/EnterpriseType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Enterprise;
+use App\Entity\Type;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,7 +20,12 @@ class EnterpriseType extends AbstractType
                 'label' => 'Nom'])
 
             ->add('email')
-            ->add('contacts');
+            ->add('contacts')
+            ->add('type', EntityType::class, [
+                'class'  => Type::class,
+                    'choice_label' => 'name'
+
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
